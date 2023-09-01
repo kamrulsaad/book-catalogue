e3c4bd60c4ea3a1d81ef91743ef14cf9d1388609
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2023-09-02 04:13:07 +0600

    prisma migration to railway database completed

--- prisma/migrations/20230901195055_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `createdAt` on the `ordered_books` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "ordered_books" DROP COLUMN "createdAt";
